1a0bdbef646f19f129ea35427a330d5d3b39a4ca
Cesar <crenteriaejr@gmail.com>
2024-01-27 10:15:22 -0800

    margin configuration

--- src/app/heading.tsx
+++ src/app/heading.tsx
@@ -1,6 +1,6 @@
 export default function Heading({text} : {text:string}){
     return(
-        <div style={{width: '97%'}} className="pt-3 border-b-2 w-full">
+        <div className="flex pt-3 border-b-2 mr-3">
             <h1 className="text-2xl font-sans font-bold my-2">{text}</h1>
         </div>
     )
